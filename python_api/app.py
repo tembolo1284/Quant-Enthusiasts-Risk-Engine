1645cbbee38fd147421e9e75ee35fff101d96859
Johannes <johannesmeyer2017@gmail.com>
2025-10-26 10:46:03 +0200

    Save submodule changes

--- python_api/app.py
+++ python_api/app.py
@@ -643,4 +643,5 @@ logger = logging.getLogger(__name__)
 logging.basicConfig(level=logging.INFO)
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000, host='0.0.0.0')
+    port = int(os.environ.get("PORT", 5000)) 
+    app.run(debug=True, host="0.0.0.0", port=port)
